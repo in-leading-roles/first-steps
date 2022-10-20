be34584939e3819332ab392422bb6b21aeb676ce
RedMarshal <redmarshal37@gmail.com>
2022-10-20 22:44:37 +0300

    Add roles module

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,19 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+interface RoleCreationAttrs{
+    value:string;
+    description:string;
+}
+
+@Table({tableName: 'roles'})
+export class Role extends Model<Role, RoleCreationAttrs>{
+    
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    value: string;
+    
+    @Column({type: DataType.STRING, allowNull: false})
+    description:string;
+}

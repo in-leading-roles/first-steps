a42df3aab386df983f9c456bd038ce61b3dea803
RedMarshall37 <81381851+RedMarshall37@users.noreply.github.com>
2022-10-20 21:30:27 +0300

    Add documentation (#6)

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class createUserDto {
+    @ApiProperty({example: 'userName', description: 'Имя пользователя'})
     readonly login: string;
+
+    @ApiProperty({example: 'user@mail.ru', description: 'Потовый адрес'})
     readonly email: string;
+
+    @ApiProperty({example: '123456789', description: 'Пароль'})
     readonly password: string;
 }

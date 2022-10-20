0d9f4fdbf41bfe05a3d8e461c7fa027f239b38ec
RedMarshall37 <81381851+RedMarshall37@users.noreply.github.com>
2022-10-20 20:09:29 +0300

    Changing env file (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { User } from "./users/users.model";
 import { UsersModule } from './users/users.module';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}`
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: 'localhost',

11d975ee1ec50fe26018e3a0a6ffcc632b0a820f
RedMarshal <redmarshal37@gmail.com>
2022-10-20 20:51:29 +0300

    Moving database data to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,15 @@ import { UsersModule } from './users/users.module';
 @Module({
     imports: [
         ConfigModule.forRoot({
-            envFilePath: `.${process.env.NODE_ENV}`
+            envFilePath: `.${process.env.NODE_ENV}.env`
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'first-steps',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             models: [User],
             autoLoadModels: true,
           }),

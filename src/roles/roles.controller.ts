056770eead8abce263c7572f22a8ab08a26a0dc9
RedMarshal <redmarshal37@gmail.com>
2022-10-21 10:59:07 +0300

    Add documentation

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,18 +1,33 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { type } from 'os';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { Role } from './roles.model';
 import { RolesService } from './roles.service';
 
+@ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
     constructor(private roleService: RolesService){}
+    
+    @ApiOperation({summary: 'Получение роли по значению'})
+    @ApiResponse({status:200, type:[Role]})
+    @Get('/:value')
+    @ApiParam({name: 'value', 
+    required: true, 
+    description: 'Значение роли',
+    example: 'USER',
+    type: 'string'
+    })
+    getByValue(@Param('value') value: string){
+        return this.roleService.getRoleByValue(value);
+    }
 
+    @ApiOperation({summary: 'Создание роли'})
+    @ApiResponse({status:200, type:Role})
     @Post()
     create(@Body() dto: CreateRoleDto){
         return this.roleService.createRole(dto);
     }
 
-    @Get('/:value')
-    getByValue(@Param('value') value: string){
-        return this.roleService.getRoleByValue(value);
-    }
 }
--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
 import { User } from "src/users/users.model";
 import { UserRoles } from "./user-roles.model";
@@ -10,12 +11,15 @@ interface RoleCreationAttrs{
 @Table({tableName: 'roles'})
 export class Role extends Model<Role, RoleCreationAttrs>{
     
+    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
+    @ApiProperty({example: 'USER', description: 'Значение роли'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     value: string;
     
+    @ApiProperty({example: 'Простой сотрудник компании', description: 'описание роли'})
     @Column({type: DataType.STRING, allowNull: false})
     description:string;
 

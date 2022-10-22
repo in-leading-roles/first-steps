ad0c7e9319e5cbaca0e2ba7b7d62080952a9fb7e
RedMarshal <redmarshal37@gmail.com>
2022-10-22 18:11:31 +0300

    Adding connection React and nest

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,13 @@ import * as React from 'react'
 import {render} from 'react-dom'
 
 const App = () => {
-    return <h1>Hello world from react</h1>
+    const [text, SetText] = React.useState();
+    React.useEffect(()=>{
+        fetch('/users')
+        .then(res => res.json())
+        .then(res => SetText(res[0].login))
+    }, [])
+    return <h1>{text}</h1>
 } 
 
 render(<App />, document.getElementById('app'));

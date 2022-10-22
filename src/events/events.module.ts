6f3598413dc8b20bd96d7461c5095f06b008eae6
DESKTOP-05DSR1O\Egor <lebedevegor1701@yandex.ru>
2022-10-22 16:51:56 +0300

    import Events from events.model to events.module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { Events } from './events.model'
+
 
 @Module({
-  // imports: [SequelizeModule.forFeature([Events])],
+  imports: [SequelizeModule.forFeature([Events])],
   providers: [EventsService],
   controllers: [EventsController]
 })

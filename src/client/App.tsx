ad82e51d79011010873f23cfb85b9b963be07d7f
RedMarshal <redmarshal37@gmail.com>
2022-10-28 00:36:04 +0300

    Fix react

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,13 +3,7 @@ import {render} from 'react-dom'
 import { UserResponse } from 'src/commmon/UserResponse';
 
 const App = () => {
-    const [text, SetText] = React.useState();
-    React.useEffect(()=>{
-        fetch('/users')
-        .then<UserResponse>(res => res.json())
-        .then(res => SetText(res[0].login))
-    }, [])
-    return <h1>{text}</h1>
+    return <h1>adafsdsada</h1>
 } 
 
 render(<App />, document.getElementById('app'));

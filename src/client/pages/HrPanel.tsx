1922ba4b4b1533afcbb11d2ba7274fbe494a3f08
DESKTOP-N9NG3DS\Пользователь <a.ozerski2@mail.ru>
2022-10-29 18:32:40 +0300

    create logout

--- src/client/pages/HrPanel.tsx
+++ src/client/pages/HrPanel.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@mui/material';
 import * as React from 'react';
-import { Calendar } from '../component/Calendar';
+import { Calendar } from '../component/Calendar'
+import { AuthContext } from '../context';
 
 const HrPanel = () => {
+    const { isAuth, setIsAuth } = React.useContext(AuthContext);
+    
+    const handlePostForm = (e: React.FormEvent) => {
+        setIsAuth(false);
+        localStorage.removeItem("auth")
+        e.preventDefault();
+        console.log(isAuth);
+      }
+
     return (
-    <Calendar/>
+        <div>
+            <Calendar/>
+            <Button onClick={handlePostForm} variant="contained">LOGOUT</Button>
+        </div>
     )
 }
 
--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -6,16 +6,12 @@ import { AuthContext } from '../context';
 
 const Login = () => {
   const { isAuth, setIsAuth } = React.useContext(AuthContext);
-  const [text, setText] = React.useState('11111111111111');
 
   const handlePostForm = (e: React.FormEvent) => {
     setIsAuth(true);
     localStorage.setItem("auth","true")
     e.preventDefault();
     console.log(isAuth);
-
-    
-    // localStorage.setItem('auth', 'true');
   }
 
   return (
@@ -27,7 +23,6 @@ const Login = () => {
         type="password"
       />
 
-      <div>{text}</div>
       <Button onClick={handlePostForm} variant="contained">LOGIN</Button>
 
       <Alert severity="success">Вы успешно зарегистрировались!</Alert>;

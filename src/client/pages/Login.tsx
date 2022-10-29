a8dd16c95b7afc8715857efd4d5c5ab6e456404c
RedMarshal <redmarshal37@gmail.com>
2022-10-29 23:38:14 +0300

    Add error detecton

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -4,49 +4,56 @@ import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginResponse } from 'src/common/LoginResponse';
 import { AuthContext } from '../context';
+import 'whatwg-fetch';
 
 const Login = () => {
   const { isAuth, setIsAuth } = React.useContext(AuthContext);
-  const [tokenLogin, setToken] = React.useState({ token: null });
   const [loginValue, setLogin] = React.useState('');
   const [passwordValue, setPassword] = React.useState('');
+  const [errorDisplay, setErrorDisplay] = React.useState('none');
 
   const handlePostForm = (e: React.FormEvent) => {
-    // let formData = new FormData();
-    // formData.append('login', loginValue);
-    // formData.append('password', passwordValue);
-
-    // console.log(formData);
-
-    fetch('http://localhost/auth/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        login: loginValue,
-        password: passwordValue,
-      }),
+    fetch('/auth/login', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ login: loginValue, password: passwordValue }),
     })
-      .then<LoginResponse>((response) => response.json())
-      .then(async (response) => setToken(await response));
-    console.log(tokenLogin['token']);
-
-    if (tokenLogin['token']) {
-      setIsAuth(true);
-      localStorage.setItem('auth', tokenLogin['token']);
-      console.log(tokenLogin['token']);
-    } else {
-      console.log('Error');
-    }
+      .then<LoginResponse>((response) => {
+        console.log('Get response');
+        return response.json();
+      })
+      .then((response) => {
+        if (response['token']) {
+          setErrorDisplay('none');
+          setIsAuth(response['token']);
+          localStorage.setItem('auth', response['token']);
+        } else {
+          setErrorDisplay('block');
+        }
+      });
     e.preventDefault();
   };
 
   return (
     <div>
-      <TextField onChange={(e) => setLogin(e.target.value)} type="text" />
       <TextField
-        onChange={(e) => setPassword(e.target.value)}
+        label="Логин"
+        onChange={(e) => setLogin(e.target.value)}
+        type="text"
+      />
+      <TextField
+        label="Пароль"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
         type="password"
       />
-
+      <Alert style={{ display: errorDisplay }} severity="error">
+        Некорректный логин или пароль
+      </Alert>
       <Button onClick={handlePostForm} variant="contained">
         LOGIN
       </Button>

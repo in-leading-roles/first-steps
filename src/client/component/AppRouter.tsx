ee0fdc7df3aeb5ca6302b74bd27061261616153d
RedMarshal <redmarshal37@gmail.com>
2022-11-07 20:03:43 +0300

    Remove useless imports from AppRouter

--- src/client/component/AppRouter.tsx
+++ src/client/component/AppRouter.tsx
@@ -1,20 +1,12 @@
 import * as React from 'react';
-import { AuthContext, RolesContext } from '../context/index';
+import { AuthContext } from '../context/index';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { privateRoutes, publicRoutes } from '../router';
 import Loader from './UI/Loader/Loader';
-import HrPanelAddUser from '../pages/HrPanelAddUser';
 import Login from '../pages/Login';
-import { Redirect } from '@nestjs/common';
-import HrPanelUsers from '../pages/HrPanelUsers';
-import HrEvents from '../pages/HrEvents';
-import HrEventsAdd from '../pages/HrEventsAdd';
-import UserMain from '../pages/UserMain';
 import AuthRouter from './AuthRouter';
 
 const AppRouter = () => {
   const { isAuth, isLoading } = React.useContext(AuthContext);
-  const { roles } = React.useContext(RolesContext);
 
   if (isLoading) {
     return <Loader />;

9f2a39a2a5249754299e63fe9d2bf4328f982f3d
RedMarshal <redmarshal37@gmail.com>
2022-11-07 20:11:52 +0300

    Clear imports in AuthRouter

--- src/client/component/AuthRouter.tsx
+++ src/client/component/AuthRouter.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import { AuthContext, RolesContext } from '../context/index';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { privateRoutes, publicRoutes } from '../router';
-import Loader from './UI/Loader/Loader';
 import HrPanelAddUser from '../pages/HrPanelAddUser';
 import Login from '../pages/Login';
-import { Redirect } from '@nestjs/common';
 import HrPanelUsers from '../pages/HrPanelUsers';
 import HrEvents from '../pages/HrEvents';
 import HrEventsAdd from '../pages/HrEventsAdd';

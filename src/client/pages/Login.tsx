a8e6e36ee01c80d8727a991f57401bf94c5cae6d
RedMarshal <redmarshal37@gmail.com>
2022-11-12 16:56:01 +0300

    remove comment from login

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -8,7 +8,6 @@ import 'whatwg-fetch';
 
 const Login = () => {
   const { isAuth, setIsAuth, roles, setRoles } = React.useContext(AuthContext);
-  // const { roles, setRoles } = React.useContext(AuthContext);
   const [loginValue, setLogin] = React.useState('');
   const [passwordValue, setPassword] = React.useState('');
   const [errorDisplay, setErrorDisplay] = React.useState('none');
